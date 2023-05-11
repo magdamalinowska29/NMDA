df97510f5cd3dc4d3733be9e9cfedf5e04306796
AndreaLoro <132842613+AndreaLoro@users.noreply.github.com>
2023-05-11 12:06:07 +0200

    Add files via upload
    Code running preprocessing. Just a draft, not functioning yet. It includes realignment, coregistration, and segmentation.

---
+++ preprocessing.m
@@ -0,0 +1,92 @@
+% List of open inputs
+nrun=1; %choose how many times to run the preprocessing
+inputs=cell(0, nrun);
+for crun=1:nrun
+end
+
+data_dir='C:\Users\Androro\Documents\Università\Methods and Data Analysis II\2023\Resources\Example data\MoAEpilot\fM00223';
+%data_dir='C:\data\fM00223\fM00223*';% chooses only files that start with fM00223
+%data_dir='C:\data\fM00223\'; %chooses all files in the folder
+
+file_list=dir(data_dir); %get a structure of files in directory
+
+file_names=strcat({file_list.folder}', filesep, {file_list.name}');
+
+% open spm
+spm('defaults', 'FMRI');
+
+%set spm parameters
+
+%set realign parameters
+matlabbatch{1}.spm.spatial.realign.estwrite.data = {file_names}';
+%%
+matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.quality = 0.9;
+matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.sep = 4;
+matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.fwhm = 5;
+matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.rtm = 1;
+matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.interp = 2;
+matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.wrap = [0 0 0];
+matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.weight = '';
+matlabbatch{1}.spm.spatial.realign.estwrite.roptions.which = [2 1];
+matlabbatch{1}.spm.spatial.realign.estwrite.roptions.interp = 4;
+matlabbatch{1}.spm.spatial.realign.estwrite.roptions.wrap = [0 0 0];
+matlabbatch{1}.spm.spatial.realign.estwrite.roptions.mask = 1;
+matlabbatch{1}.spm.spatial.realign.estwrite.roptions.prefix = 'r';
+
+matlabbatches= repmat(matlabbatch, 1, nrun); 
+
+%set coregistration parameters
+matlabbatch{1}.spm.spatial.coreg.estwrite.ref = {'C:\Users\Androro\Documents\Università\Methods and Data Analysis II\2023\Resources\Example data\MoAEpilot\fM00223\rfM00223_004.img,1'};
+matlabbatch{1}.spm.spatial.coreg.estwrite.source = {'C:\Users\Androro\Documents\Università\Methods and Data Analysis II\2023\Resources\Example data\MoAEpilot\sM00223\sM00223_002.img,1'};
+matlabbatch{1}.spm.spatial.coreg.estwrite.other = {''};
+matlabbatch{1}.spm.spatial.coreg.estwrite.eoptions.cost_fun = 'nmi';
+matlabbatch{1}.spm.spatial.coreg.estwrite.eoptions.sep = [4 2];
+matlabbatch{1}.spm.spatial.coreg.estwrite.eoptions.tol = [0.02 0.02 0.02 0.001 0.001 0.001 0.01 0.01 0.01 0.001 0.001 0.001];
+matlabbatch{1}.spm.spatial.coreg.estwrite.eoptions.fwhm = [7 7];
+matlabbatch{1}.spm.spatial.coreg.estwrite.roptions.interp = 4;
+matlabbatch{1}.spm.spatial.coreg.estwrite.roptions.wrap = [0 0 0];
+matlabbatch{1}.spm.spatial.coreg.estwrite.roptions.mask = 0;
+matlabbatch{1}.spm.spatial.coreg.estwrite.roptions.prefix = 'r';
+
+%set segmentation parameters
+matlabbatch{1}.spm.spatial.preproc.channel.vols = {'C:\Users\Androro\Documents\Università\Methods and Data Analysis II\2023\Resources\Example data\MoAEpilot\sM00223\sM00223_002.img,1'};
+matlabbatch{1}.spm.spatial.preproc.channel.biasreg = 0.001;
+matlabbatch{1}.spm.spatial.preproc.channel.biasfwhm = 60;
+matlabbatch{1}.spm.spatial.preproc.channel.write = [0 1];
+matlabbatch{1}.spm.spatial.preproc.tissue(1).tpm = {'C:\Program Files\Matlab\spm12\spm12\tpm\TPM.nii,1'};
+matlabbatch{1}.spm.spatial.preproc.tissue(1).ngaus = 1;
+matlabbatch{1}.spm.spatial.preproc.tissue(1).native = [1 0];
+matlabbatch{1}.spm.spatial.preproc.tissue(1).warped = [0 0];
+matlabbatch{1}.spm.spatial.preproc.tissue(2).tpm = {'C:\Program Files\Matlab\spm12\spm12\tpm\TPM.nii,2'};
+matlabbatch{1}.spm.spatial.preproc.tissue(2).ngaus = 1;
+matlabbatch{1}.spm.spatial.preproc.tissue(2).native = [1 0];
+matlabbatch{1}.spm.spatial.preproc.tissue(2).warped = [0 0];
+matlabbatch{1}.spm.spatial.preproc.tissue(3).tpm = {'C:\Program Files\Matlab\spm12\spm12\tpm\TPM.nii,3'};
+matlabbatch{1}.spm.spatial.preproc.tissue(3).ngaus = 2;
+matlabbatch{1}.spm.spatial.preproc.tissue(3).native = [1 0];
+matlabbatch{1}.spm.spatial.preproc.tissue(3).warped = [0 0];
+matlabbatch{1}.spm.spatial.preproc.tissue(4).tpm = {'C:\Program Files\Matlab\spm12\spm12\tpm\TPM.nii,4'};
+matlabbatch{1}.spm.spatial.preproc.tissue(4).ngaus = 3;
+matlabbatch{1}.spm.spatial.preproc.tissue(4).native = [1 0];
+matlabbatch{1}.spm.spatial.preproc.tissue(4).warped = [0 0];
+matlabbatch{1}.spm.spatial.preproc.tissue(5).tpm = {'C:\Program Files\Matlab\spm12\spm12\tpm\TPM.nii,5'};
+matlabbatch{1}.spm.spatial.preproc.tissue(5).ngaus = 4;
+matlabbatch{1}.spm.spatial.preproc.tissue(5).native = [1 0];
+matlabbatch{1}.spm.spatial.preproc.tissue(5).warped = [0 0];
+matlabbatch{1}.spm.spatial.preproc.tissue(6).tpm = {'C:\Program Files\Matlab\spm12\spm12\tpm\TPM.nii,6'};
+matlabbatch{1}.spm.spatial.preproc.tissue(6).ngaus = 2;
+matlabbatch{1}.spm.spatial.preproc.tissue(6).native = [0 0];
+matlabbatch{1}.spm.spatial.preproc.tissue(6).warped = [0 0];
+matlabbatch{1}.spm.spatial.preproc.warp.mrf = 1;
+matlabbatch{1}.spm.spatial.preproc.warp.cleanup = 1;
+matlabbatch{1}.spm.spatial.preproc.warp.reg = [0 0.001 0.5 0.05 0.2];
+matlabbatch{1}.spm.spatial.preproc.warp.affreg = 'mni';
+matlabbatch{1}.spm.spatial.preproc.warp.fwhm = 0;
+matlabbatch{1}.spm.spatial.preproc.warp.samp = 3;
+matlabbatch{1}.spm.spatial.preproc.warp.write = [0 1];
+matlabbatch{1}.spm.spatial.preproc.warp.vox = NaN;
+matlabbatch{1}.spm.spatial.preproc.warp.bb = [NaN NaN NaN
+                                              NaN NaN NaN];
+
+%run preprocessing
+spm_jobman('run', matlabbatches, inputs{:});
