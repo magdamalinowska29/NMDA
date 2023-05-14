7cd57f84578e03469dc34554723343450fc49f14
maridazhupa <131866065+maridazhupa@users.noreply.github.com>
2023-05-14 13:26:10 +0200

    smoothing.m
    This function takes as input the path to the folder with the normalized images (functional). Smoothing is typically applied to normalized functional data rather than structural data in fMRI analysis to enhance the functional signal, reduce spatial noise, and improve the sensitivity of detecting brain activity.
     It performs spatial smoothing on the data using a Gaussian kernel with a FWHM of 8 mm. The smoothed images are saved with the prefix 's'.

--- smooth_images.m
+++ smooth_images.m
@@ -1,4 +1,4 @@
-function results = smooth_images(data_dir)
+function results = smoothing(data_dir)
 % This function takes as input the path to the folder with the normalized images (functional or structural).
 % It performs spatial smoothing on the data using a Gaussian kernel with a FWHM of 8 mm.
 % The smoothed images are saved with the prefix 's'.
@@ -14,21 +14,22 @@ if ~exist('data_dir','var')
    data_dir = pwd;
 end
 
-% Find the normalized images
-data_dir = strcat(data_dir, 'w*'); % here we specify to only smooth normalized data files that start with 'w'.
+% get the normalised functional images
+data_dir_norm = strcat(data_dir, 'w*'); %here we specify to only use normalised functional images that start with w
 
-file_list = dir(data_dir); % get a structure of image files in the directory
+file_list_norm = dir(data_dir_norm); %get a structure of image files in directory
 
-file_names = strcat({file_list.folder}', filesep, {file_list.name}');
+file_names_norm = strcat({file_list_norm.folder}', filesep, {file_list_norm.name}');
 
 % Set spm parameters
-matlabbatch{1}.spm.spatial.smooth.data = file_names; % Specify the file names of the normalized images to be smoothed.
+matlabbatch{1}.spm.spatial.smooth.data = file_names_norm; % Specify the file names of the normalized images to be smoothed.
 matlabbatch{1}.spm.spatial.smooth.fwhm = [8 8 8]; % Set the Full Width at Half Maximum (FWHM) of the Gaussian smoothing kernel to 8 mm in each dimension.
 matlabbatch{1}.spm.spatial.smooth.dtype = 0; % Set the data type of the smoothed images to the same as the input.
 matlabbatch{1}.spm.spatial.smooth.im = 0; % Perform implicit masking during smoothing, where zero values are excluded from the smoothing.
 matlabbatch{1}.spm.spatial.smooth.prefix = 's'; % Set the prefix for the smoothed images to 's'.
 
-matlabbatches = repmat(matlabbatch, 1, nrun); 
+% run smoothing
+matlabbatches = repmat(matlabbatch, 1, nrun);
 
-% Run smoothing
 spm_jobman('run', matlabbatches, inputs{:});
+disp('Smoothing completed.');
