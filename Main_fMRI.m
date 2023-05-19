7ce4961cf6f0317a26ec025db30540b6e70eec5e
magdamalinowska29 <73298429+magdamalinowska29@users.noreply.github.com>
2023-05-19 14:28:15 +0200

    Main file with setups, calls functions with processing steps
    Now the file only calls for the 'preprocessing' function which contains all preprocessing steps

--- Main_fMRI.m
+++ Main_fMRI.m
@@ -12,25 +12,31 @@ clear; %clear the workspace
 %providing your path, keep in mind that processing functions search for
 %proper files by concatenating the path with name patter e.g 'C:\data\r*'
 
-
-whichComp=3;
+whichComp=2;
 
 if whichComp==1
     spmPath='C:\Users\Marida\Documents\MATLAB\spm12';
-    data_dir_functional='C:\Users\Marida\Documents\MATLAB\spm12';
-    %data_dir_structural=
+    data_path='C:\data\auditory' % We specify only one data_path where fM00223 and sM00223 are located, no need two write separately
+    tpm_path = 'C:\Users\Marida\Documents\MATLAB\spm12\tpm\TPM.nii' 
+
+% By providing the tpm_path,we ensure that the script can locate the necessary file for segmentation step
+% regardless of the user running the code, without requiring manual modification of the file path. 
+    
 elseif whichComp==2
     spmPath='C:\';
-    data_dir_functional='C:\data\fM00223\'; %directory of functional files
-    data_dir_structural='C:\data\sM00223\';%directory of structural files
-elseif whichcomp==3
-    spmPath='C:\Program Files\Matlab\spm12';
-    data_dir_functional=['C:\Users\Androro\Documents\Università\Methods and ' ...
-        'Data Analysis II\2023\Resources\Example data\MoAEpilot\fM00223']; %directory of functional files
-    data_dir_structural=['C:\Users\Androro\Documents\Università\Methods ' ...
-        'and Data Analysis II\2023\Resources\Example data\MoAEpilot\sM00223'];%directory of structural files
-    %add one more option
+    data_path='C:\data\';
+    tpm_path='C:\spm12\tpm\TPM.nii';
+elseif whichComp==3 
+    spmPath='/Users/USERNAME/WHERE/spm12';
+    data_path='ADD';
+    tpm_path='Add';
+else
+    spmPath='/Users/USERNAME/WHERE/spm12';
+    data_path='ADD';
+    tpm_path='Add';
 end
+
+addpath(spmPath)
 
 %This script relies heavily on the default naming pattern of spm. If files
 %you want to use have different names, you'll have to change the function
@@ -43,19 +49,4 @@ spm('defaults', 'FMRI');
 
 %run preprocessing
 
-%run realignment
-
-realignment(data_dir_functional); 
-
-%coregistration
-
-coregistration(data_dir_structural,data_dir_functional)
-
-%normalisation
-
-normalise_functional(data_dir_structural,data_dir_functional); 
-
-normalise_structural(data_dir_structural);
-
-%smooting
-smoothing(data_dir_functional);
+preprocessing(data_path,tpm_path)
