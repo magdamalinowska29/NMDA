08f75590a1ed359026eab554da6523f157ee3dcd
magdamalinowska29 <73298429+magdamalinowska29@users.noreply.github.com>
2023-06-10 16:19:56 +0200

    now performs 2nd level glm
    added function calls for two second level glm functions, One-sample ttest and 2x4 factorial design; note you have to provide right directories and other arguments for the functions to run properly

--- Main_fMRI.m
+++ Main_fMRI.m
@@ -16,9 +16,13 @@ whichComp=2;
 
 if whichComp==1
     spmPath='C:\Users\Marida\Documents\MATLAB\spm12';
-    data_path='C:\data\auditory\' % We specify only one data_path where fM00223 and sM00223 are located, no need two write separately
+    data_path='C:\data\auditory\'; % We specify only one data_path where fM00223 and sM00223 are located, no need two write separately
     tpm_path = 'C:\Users\Marida\Documents\MATLAB\spm12\tpm\TPM.nii'; 
-    glm_dir='C:\data\auditory\classical';
+    glm_dir='ADD';
+    glm2_dir='ADD';
+    glm2_sub_folders='ADD';
+    one_sample_ttest_input_pattern= '^con_0009\.img$';
+    Anova_2x4_file_patterns={'^con_0009\.img$', '^con_0010\.img$', '^con_0011\.img$', '^con_0012\.img$', '^con_0013\.img$', '^con_0014\.img$', '^con_0015\.img$', '^con_0016\.img$'};
 
 % By providing the tpm_path,we ensure that the script can locate the necessary file for segmentation step
 % regardless of the user running the code, without requiring manual modification of the file path. 
@@ -28,6 +32,11 @@ elseif whichComp==2
     data_path='C:\data\';
     tpm_path='C:\spm12\tpm\TPM.nii';
     glm_dir='C:\data\auditory\classical';
+    glm2_dir='C:\data\SomaTI-Example\RFX1';
+    glm2_sub_folders='C:\data\SomaTI-Example';
+    one_sample_ttest_input_pattern= '^con_0009\.img$';
+    Anova_2x4_file_patterns={'^con_0009\.img$', '^con_0010\.img$', '^con_0011\.img$', '^con_0012\.img$', '^con_0013\.img$', '^con_0014\.img$', '^con_0015\.img$', '^con_0016\.img$'};
+
 elseif whichComp==3 
     spmPath='/Users/USERNAME/WHERE/spm12';
     data_path='ADD';
@@ -53,7 +62,17 @@ spm('defaults', 'FMRI');
 
 %run preprocessing
 
-%preprocessing(data_path,tpm_path);
+preprocessing(data_path,tpm_path);
 
 %run first level GLM
 GLM(data_path,glm_dir);
+
+%run 2nd level GLM, one-sample t-test
+
+One_sample_ttest_2nd_level(glm2_dir,glm2_sub_folders,one_sample_ttest_input_pattern)
+
+% Anova 2x4 Mains+interaction
+
+anova_2x4_Main_Interaction(glm2_dir, glm2_sub_folders, Anova_2x4_file_patterns)
+
+
